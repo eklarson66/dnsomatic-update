04486a33a499e8dcaddf4fe3459b9e7ae02e1496
eklarson66 <55821583+eklarson66@users.noreply.github.com>
2023-01-24 13:45:43 -0700

    Update dnsomatic-update.py

--- dnsomatic-update.py
+++ dnsomatic-update.py
@@ -11,7 +11,7 @@ from time import strftime, sleep
 # mandatory vars
 USERID = os.getenv('USERID')
 PASSWORD = os.getenv('PASSWORD')
-INTERVAL = os.getenv('INTERVAL', 300)
+INTERVAL = os.getenv('INTERVAL', 900)
 HOST = os.getenv('HOST', 'all.dnsomatic.com')
 WILDCARD = os.getenv('WILDCARD', 'NOCHG')
 MX = os.getenv('MX', 'NOCHG')
